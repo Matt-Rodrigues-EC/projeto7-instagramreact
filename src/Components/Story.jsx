c767b672559bea5d705aaaccc60c0e3e937401ec
Matt Rodrigues <ec.mattrodrigues@gmail.com>
2023-02-09 14:14:01 -0300

    Finish Navbar and Create Stories Components

---
+++ src/Components/Story.jsx
@@ -0,0 +1,54 @@
+
+let Images = [
+    {
+        image: 'Images/Profiles/LogoGatastico.png',
+        name: 'Gatastico'
+    },
+    {
+        image: 'Images/Profiles/LogoDoggie.jpg',
+        name: 'Doggie'
+    },
+    {
+        image: 'Images/Profiles/LogoHusky.jpg',
+        name: 'ILoveHusky'
+    },
+    {
+        image: 'Images/Profiles/LogoJuliano.jpg',
+        name: 'Juliano'
+    },
+    {
+        image: 'Images/Profiles/LogoLobo.jpg',
+        name: 'Lobo'
+    },
+    {
+        image: 'Images/Profiles/LogoMeuLoto.jpeg',
+        name: 'MeuLoro'
+    },
+    {
+        image: 'Images/Profiles/Miaugnifico.jpg',
+        name: 'Miaugnifico'
+    },
+    {
+        image: 'Images/Profiles/LogoOrca.jpg',
+        name: 'Orca'
+    },
+    {
+        image: 'Images/Profiles/LogoTatuDoBem.jpg',
+        name: 'TatuDoBem'
+    }
+]
+
+export default function Story(){
+
+
+    return (
+        Images.map((story) => {
+            return(
+                <div className="Story">
+                    <img src={story.image} className="StoryIMG" alt=""/>
+                    <p className="NameStory">{story.name}</p>
+                </div>
+                )
+        })
+    )
+}
---
+++ src/Components/StoryBox.jsx
@@ -0,0 +1,9 @@
+import Story from "./Story"
+
+export default function StoryBox() {
+    return (
+        <div className="StoryBox">
+            <Story/>
+        </div>
+    )
+}
